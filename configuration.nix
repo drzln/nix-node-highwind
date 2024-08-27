6e2c78b83cd5e0f4add31e3b7adc4e89dfb25e60
luis <ldesiqueira@pinger.com>
2024-08-27 18:32:25 -0300

    init

--- configuration.nix
+++ configuration.nix
@@ -1,29 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "plo"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "highwind";
   time.timeZone = "America/Fortaleza";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -38,71 +18,12 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  services.libinput = { enable = true; };
-  services.displayManager = {
-    defaultSession = "gnome";
-    sddm = {
-      enable = false;
-      theme = "nord";
-    };
-    # defaultSession = "none+i3";
-    # defaultSession = "none+leftwm";
-  };
-
-  services.xserver = {
-    enable = false;
-    xkb = {
-      options = "caps:escape";
-      layout = "us";
-      variant = "";
-    };
-    displayManager = {
-      gdm = {
-        enable = true;
-        wayland = false;
-      };
-      lightdm = {
-        enable = false;
-      };
-    };
-    autoRepeatDelay = 135;
-    autoRepeatInterval = 40;
-    videoDrivers = [ "nvidia" "intel" "nouveau" "qxl" "amdgpu" ];
-    desktopManager = {
-      gnome = {
-        enable = false;
-      };
-      xterm = {
-        enable = false;
-      };
-    };
-    windowManager = {
-      leftwm = { enable = false; };
-      i3 = {
-        enable = false;
-        extraPackages = with pkgs; [
-          i3blocks
-          i3status
-          i3lock
-          dmenu
-        ];
-      };
-    };
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.luis = {
     uid = 1000;
     isNormalUser = true;
     description = "luis";
     extraGroups = [
-      "networkmanager"
       "wheel"
-      "docker"
-      "podman"
-      "libvirtd"
-      "audio"
-      "video"
     ];
     packages = with pkgs; [
       home-manager
@@ -110,12 +31,6 @@
   };
 
   home-manager.users.luis = import ./home.nix;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     nix-index
     drm_info
@@ -166,17 +81,6 @@
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
   services.openssh.settings.PasswordAuthentication = true;
@@ -187,13 +91,6 @@
     "8.8.8.8"
     "8.8.4.4"
   ];
-  services.blueman.enable = true;
-  services.printing.enable = false;
-  services.hardware.bolt.enable = false;
-  services.nfs.server.enable = false;
-  services.pipewire.enable = true;
-
-  security.rtkit.enable = true;
 
   fonts.packages = with pkgs;[
     fira-code
@@ -217,27 +114,10 @@
     package = pkgs.nixFlakes;
   };
 
-  console = { font = "Lat2-Terminus16"; keyMap = "us"; };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = false;
   networking.firewall.extraCommands = ''
     ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
   '';
-  networking.wireless.interfaces = [ "wlp0s20f3" ];
-
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.settings.dns_enabled = true;
-  virtualisation.docker.enable = false;
-  virtualisation.docker.rootless = {
-    setSocketVariable = true;
-    enable = true;
-  };
-  virtualisation.libvirtd.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
