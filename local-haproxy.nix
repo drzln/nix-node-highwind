c32ce7b09e2037f11378d04f669d5804a271541d
luis <ldesiqueira@pinger.com>
2024-08-28 14:47:20 -0300

    init

--- local-haproxy.nix
+++ local-haproxy.nix
@@ -1,26 +1,26 @@
 { config, pkgs, ... }: {
-  systemd.services.haproxy = {
-    description = "HAProxy Load Balancer";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.haproxy}/bin/haproxy -f /etc/haproxy/haproxy.cfg";
-      ExecReload = "${pkgs.haproxy}/bin/haproxy -c -f /etc/haproxy/haproxy.cfg && ${pkgs.haproxy}/bin/haproxy -sf $MAINPID";
-      Restart = "always";
-      User = "haproxy";
-      Group = "haproxy";
-      PIDFile = "/run/haproxy.pid";
-    };
-
-    path = [ pkgs.haproxy ];
-
-    # Optionally, create the /run/haproxy directory
-    preStart = ''
-      mkdir -p /run/haproxy
-      chown haproxy:haproxy /run/haproxy
-    '';
-  };
+  # systemd.services.haproxy = {
+  #   description = "HAProxy Load Balancer";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.haproxy}/bin/haproxy -f /etc/haproxy/haproxy.cfg";
+  #     ExecReload = "${pkgs.haproxy}/bin/haproxy -c -f /etc/haproxy/haproxy.cfg && ${pkgs.haproxy}/bin/haproxy -sf $MAINPID";
+  #     Restart = "always";
+  #     User = "haproxy";
+  #     Group = "haproxy";
+  #     PIDFile = "/run/haproxy.pid";
+  #   };
+  #
+  #   path = [ pkgs.haproxy ];
+  #
+  #   # Optionally, create the /run/haproxy directory
+  #   preStart = ''
+  #     mkdir -p /run/haproxy
+  #     chown haproxy:haproxy /run/haproxy
+  #   '';
+  # };
 
   environment.etc."haproxy/haproxy.cfg".text = ''
     global
