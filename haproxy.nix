b4f500ce6f030854246303d78579a86736739fe5
luis <ldesiqueira@pinger.com>
2024-08-28 14:39:35 -0300

    init

--- haproxy.nix
+++ haproxy.nix
@@ -3,6 +3,27 @@
     enable = true;
     # package = pkgs.haproxy;
 
+    config = ''
+      global
+          log /dev/log local0
+          chroot /var/lib/haproxy
+          user haproxy
+          group haproxy
+          daemon
+
+      defaults
+          log global
+          timeout connect 5000ms
+          timeout client  50000ms
+          timeout server  50000ms
+
+      frontend http-in
+          bind *:8089
+          default_backend servers
+
+      backend servers
+          server server1 127.0.0.1:8080 maxconn 32
+    '';
     # config = ''
     #   global
     #       log /dev/log local0
