9a4110bbf20563376c7a58a322d33a49db6b1bc2
luis <ldesiqueira@pinger.com>
2024-08-28 12:47:58 -0300

    init

--- home.nix
+++ home.nix
@@ -17,6 +17,6 @@
 
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
-  blackmater.programs.nvim.enable = true;
+  blackmatter.programs.nvim.enable = true;
   # home.file.".config/sheldon/plugins.toml".source = ./sheldon/plugins.toml;
 }
