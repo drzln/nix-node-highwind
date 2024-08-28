5743c748b0010f07d3ceffcaeb40c6781ddd8c7f
luis <ldesiqueira@pinger.com>
2024-08-28 13:24:42 -0300

    init

--- home.nix
+++ home.nix
@@ -25,5 +25,6 @@
 
   home.sessionVariables.SHELL = "${pkgs.zsh}/bin/zsh";
   blackmatter.programs.nvim.enable = true;
+  blackmatter.programs.shell.enable = true;
   # home.file.".config/sheldon/plugins.toml".source = ./sheldon/plugins.toml;
 }
